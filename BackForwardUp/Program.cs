547f61c543c5093493477ea16a338f39e31761cf
SukharevMA <maxs130872@gmail.com>
2022-07-20 20:24:54 +0700

    Создание решения

---
+++ BackForwardUp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BackForwardUp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
